4ed736b5532790f2fe56290ee00557d62843444d
Joon-Young Lee <leejykw2025@office.kw.ac.kr>
2026-06-10 14:28:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
-
+import uvicorn
 app = FastAPI()
 
 @app.get("/")
 def index():
     return {"Hello": "World"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
